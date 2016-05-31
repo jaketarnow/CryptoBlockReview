72bc1159a2153f5342533ab510239ee991c13e68
jaketarnow <jtarnow@dons.usfca.edu>
2016-05-31 14:49:10 -0700

    adding comments

--- Crypto/CryptoOne.java
+++ Crypto/CryptoOne.java
@@ -1,6 +1,17 @@
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/*
+Given a variable integer X (e.g. 42) and a string word (e.g. "hello"),
+where word is iteratively run through a SHA-256 hex digest computation X number of times,
+what is the resulting hex digest?
+
+So the answer for X = 1 and word = "hello" would be "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824".
+
+For X = 2 and word = "bitcoin", the answer would be "a23b7f87e4250b3a64b737f349c06422f752f419cbb25ae9169a6cf1e23f4462".
+
+Please provide a method that can compute the general solution.
+ */
 public class CryptoOne {
     public static String getHash(String word, int x) throws NoSuchAlgorithmException {
         String oldWord = word;
